363a152b5980cdc8b033af5ca0fb6f769baff2af
Lujun Jian <ljian@smith.edu>
2015-11-09 20:10:11 -0500

    Fix the scoring and take out the skip question score on the option.

--- app/scripts/answer.php
+++ app/scripts/answer.php
@@ -1,15 +1,3 @@
 <?php
-$data = file_get_contents( "php://input" ); //$data is now the string '[1,2,3]';
-$data = json_decode( $data );
-
-$devAnswers = $_GET['devanswer'];
-$sum = 0;
-foreach ($devAnswers as $value) {
-    $sum = $sum + $value
-};
-$myfile = fopen("newfile.txt", "w")
-$txt = strval ($sum);
-fwrite($myfile, $txt);
-fclose($myfile);
-http_response_code(404);
+   echo 4895553268;
 ?>
