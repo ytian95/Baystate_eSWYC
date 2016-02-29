e28ed9a6131d63ee9a11b8796bc1a0c939b6c700
aupreti <aupreti@smith.edu>
2016-02-29 02:26:49 -0500

    Generate json file with php

--- app/send_json.php
+++ app/send_json.php
@@ -2,7 +2,17 @@
    $json = file_get_contents("php://input");
    $_POST = json_decode($json, true);
    $content = json_encode($_POST);
-   file_put_contents('patient1.json', $content);
+
+   $clean = array();
+   $jsonclean = json_encode($clean);
+   //file_put_contents('patient1.json', $jsonclean);
+   
+   //$fp = fopen('/patient_folder/sample_1231231234.json', 'w');
+   //fclose($fp);
+   
+   //file_put_contents('patient1.json', $content);
+   file_put_contents('patient_folder/sample2_5231231234.json', $content);
+   echo "Get in PHP!";
    //$mobile = array('sam',"apple");
    //$jsonm = json_encode($mobile);
    //file_put_contents('patient1.json', $jsonm);
