54e952e1aa9ed5f617838e7fce8b1f093248f57c
aupreti <aupreti@smith.edu>
2016-03-02 03:20:18 -0500

    Generate json for each patient

--- app/send_json.php
+++ app/send_json.php
@@ -1,12 +1,18 @@
 <?php
    $json = file_get_contents("php://input");
    $_POST = json_decode($json, true);
+
+   $name = $_POST["name"];
+   $phone = $_POST["phone"];
+
+   $filename = "patient_folder/" . $name . "_" . $phone . ".json";
+   echo $filename;
    
-   $content = json_encode($_POST);
+   $content = json_encode($_POST["info"]);
 
    $clean = array();
    $jsonclean = json_encode($clean);
 
-   file_put_contents('patient_folder/sample2_5231231234.json', $content);
-   echo "Get in PHP!";
+   file_put_contents($filename, $content);
+
 ?>
