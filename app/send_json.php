6387035c55dc9269205e48a899f64f857b83a039
aupreti <aupreti@smith.edu>
2016-03-01 21:31:07 -0500

    Clean nits

--- app/send_json.php
+++ app/send_json.php
@@ -1,19 +1,12 @@
 <?php
    $json = file_get_contents("php://input");
    $_POST = json_decode($json, true);
+   
    $content = json_encode($_POST);
 
    $clean = array();
    $jsonclean = json_encode($clean);
-   //file_put_contents('patient1.json', $jsonclean);
-   
-   //$fp = fopen('/patient_folder/sample_1231231234.json', 'w');
-   //fclose($fp);
-   
-   //file_put_contents('patient1.json', $content);
+
    file_put_contents('patient_folder/sample2_5231231234.json', $content);
    echo "Get in PHP!";
-   //$mobile = array('sam',"apple");
-   //$jsonm = json_encode($mobile);
-   //file_put_contents('patient1.json', $jsonm);
 ?>
